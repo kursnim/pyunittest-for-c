0f73fe06b90b3c4f45ef7e911fab774dfb719832
kursnim <kursnim@gmail.com>
2021-01-20 00:38:48 +0900

    ex2

--- example2_HelloWorld/build_and_test.py
+++ example2_HelloWorld/build_and_test.py
@@ -4,5 +4,13 @@ ffibuilder.set_source("py_HelloWorld",'#include <stdio.h>')
 ffibuilder.cdef("int printf(const char *format, ...);")
 ffibuilder.compile()
 
-from py_HelloWorld.lib import printf
-printf(b"Hello, world!\n")
+from py_HelloWorld import ffi
+lib = ffi.dlopen()
+
+#import ctypes.util         # or, try this on Windows:
+#lib = ffi.dlopen(ctypes.util.find_library("c"))
+
+number = 123
+
+lib.printf(b"Hello, world!\n")
+lib.printf(b"int : %d\n", ffi.cast('int', number))
